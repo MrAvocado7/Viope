06ea2bbace4bfd21888a4cf74c816b8a48e049e1
bhq077@myy.haaga-helia.fi <bhq077@myy.haaga-helia.fi>
2024-09-27 11:37:53 +0300

    new ex

--- Viope/Javascript Chapter 2/02 - One Button/04002OneButtonEx.js
+++ Viope/Javascript Chapter 2/02 - One Button/04002OneButtonEx.js
@@ -2,6 +2,9 @@ function logPressing() {
     console.log("Button Pressed.");
     return 0;
 }
+function buttonClicked() {
+    console.log("Hello!");
+}
 const button = document.getElementById('MyButton');
 button.addEventListener('click', function() {
     logPressing();
