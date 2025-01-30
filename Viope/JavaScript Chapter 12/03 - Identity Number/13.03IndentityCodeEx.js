059934e8437a10043da28d2d000b6143b53cc6c3
MrAvocado7 <salva.finl2017@gmail.com>
2025-01-30 11:17:27 +0200

    exercises

---
+++ Viope/JavaScript Chapter 12/03 - Identity Number/13.03IndentityCodeEx.js
@@ -0,0 +1,19 @@
+// function shows details about Identity Number
+function show() {
+    // Read value of identity number from the input field
+   
+
+    //Check gender
+    // Get the second last character with charAt() method 
+    // Convert digit text to number with parseInt() function
+    // Check gender with modulo (%) operation (even number-> female, odd number-> male)
+    
+
+    // Parse birth day information
+    // Take certain characters (dd, mm, yy and separator character)) from the string with substring() method 
+    // Calculate the year with four digits
+    // Concatenate birth day text
+
+    // Write the answer on the page, to the answer div, as content of the div
+    
+}
---
+++ Viope/JavaScript Chapter 12/04 - Key Codes/13.04KeyCodesEx.js
@@ -0,0 +1,30 @@
+// Function returns the four digit key codes between min-max 
+function generateKeyCode(min, max) {
+   
+    // Randomize the key code with Math.random() function
+   
+
+    // Convert a number to a string with the toString() method.
+    // Use the length property of the text string.
+    
+    // Add leading zeros to keyCode, if needed (eg. 9 -> 0009)
+    // with loop
+    
+    // Return four digit key code as a text
+    
+}
+
+
+// For hundred times
+
+    // Call generateKeyCode() function 
+    // Print key code to the document
+    
+    // If ordinal number of the keyCode MODULO (%) 10 equals zero
+
+        // Print html line break to the document
+        
+    }
+}
+//Show result in html page (div with id value "keycodes")
+
