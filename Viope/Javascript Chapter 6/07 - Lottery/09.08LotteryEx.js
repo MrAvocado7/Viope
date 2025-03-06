4c2b405373dfaa1452b102cd0a2fd163da3744b3
MrAvocado7 <salva.finl2017@gmail.com>
2025-03-06 15:27:59 +0200

    chapter 6 done

--- Viope/Javascript Chapter 6/07 - Lottery/09.08LotteryEx.js
+++ Viope/Javascript Chapter 6/07 - Lottery/09.08LotteryEx.js
@@ -1,26 +1,34 @@
-let names = [];
+// The array variable names is declared inside the script element, but NOT inside the function. 
+// The array is availabe for both functions to read from or write to.
+let names = []; // declaring a variable and creating empty array object
 
 function addName() {
-    
-    let name = document.getElementById("name").value;
-    while (name) {
-        names.push(name);
+	// Assign name from the input field to the array (use push())
+	let name = document.getElementById("name").value;
+	names.push(name);
+	// Go through the array in a for loop adding each name at the end of the namesText
+	let namesText = "Inserted names: ";
+	for (let i = 0; i < names.length; i++) {
+		namesText += names [i];
+		if (i < names.length - 1) {
+			namesText += ", ";
+		}
+	}
+	// Write the names on the page, to the names div, as content of the div
+	document.getElementById("nameList").innerText = namesText;
 
-        let namesText = "inserted names: " + names.join(" ");
+	// Empty the input field of name as that name was just put in the array
+	document.getElementById("name").value = "";
 
-        document.getElementById("names").innerText = namesText;
-
-        document.getElementById("answer").innerText = "Inserted names: " + answer;
-
-    }
+	// Empty the answer text
+	document.getElementById("answer").innerText = "";
 }
 
 function makeDraw() {
-    
-    while (names.length > 0) {
-        let winningIndex = Math.floor(Math.random() * names.length);
-        let winner = names[winningIndex];
 
-        document.getElementById("answer").innerText = `Winner is ${winner}`;
-    }
+	// Randomize the index of the winner
+	let winningIndex = Math.floor(Math.random() * names.length);
+	
+	// Write the answer on the page, to the answer div, as content of the div
+	document.getElementById("answer").innerText = "Winner is: " + names[winningIndex];
 }
